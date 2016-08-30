1ce66ca5141bc3e34b93598d36069f743192775e
Yannick <yannick.darcillon@gmail.com>
2016-08-30 14:50:03 +0200

    first commit

---
+++ TennisConsoleApplication/ITennisScorer.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    namespace Tennis
+    {
+        public interface ITennisScorer
+        {
+            void AchievesScore(Player player);
+            string GameScore { get; }
+        }
+
+        public enum Player
+        {
+            PlayerA,
+            PlayerB
+        }
+
+        public enum Score
+        {
+            Love    = 0,
+            Fifteen = 1,
+            Thirty  = 2,
+            Forty   = 3
+        }
+
+
+    public class TennisGame : ITennisScorer
+    {
+
+        public TennisGame()
+        {
+            this._gameScorePlayerA = 0;
+            this._gameScorePlayerB = 0;
+        }
+
+        private int _gameScorePlayerA;
+        private int _gameScorePlayerB;
+
+        public string GameScore
+        {
+            get
+            {
+                var score = string.Empty;
+
+                if (HasWinner())
+                {
+                   return ( "Win " + (_gameScorePlayerA - _gameScorePlayerB > 1 ? "PlayerA" : "PlayerB") );
+                }
+
+                else if (_gameScorePlayerA >= 3 && _gameScorePlayerB >= 3 && _gameScorePlayerA != _gameScorePlayerB)
+                {
+                    return (getAdvencedScore(score));
+                }
+
+                 return ( GetLitteralScore(_gameScorePlayerA).ToString() + " - " + (_gameScorePlayerA == _gameScorePlayerB ? "All" : GetLitteralScore(_gameScorePlayerB).ToString()) );
+            }
+        }
+
+        private bool HasWinner()
+        {
+            if ( (_gameScorePlayerA >= 3 || _gameScorePlayerB >= 3) && Math.Abs(_gameScorePlayerA - _gameScorePlayerB)>1)
+            {
+                return true;
+            }
+            return false;
+       }
+
+        private string getAdvencedScore(string score)
+        {
+             if (_gameScorePlayerA > _gameScorePlayerB)
+            {
+                score = "Advantage PlayerA";
+            }
+
+            else if ( _gameScorePlayerB > _gameScorePlayerA)
+            {
+                score = "Advantage PlayerB";
+            }
+
+            return score;
+        }
+
+        public string GetPlayerScore(Player player)
+        {
+            return string.Empty;
+        }
+
+        private Score GetLitteralScore(int x)
+        {
+            switch (x)
+            {
+                case 0:
+                    return Score.Love;
+                case 1:
+                    return Score.Fifteen;
+                case 2:
+                    return Score.Thirty;
+                case 3:
+                    return Score.Forty;
+                default:
+                    return Score.Forty;
+            }
+        }
+
+        public void AchievesScore(Player player)
+        {
+            if (player == Player.PlayerA)
+            {
+                _gameScorePlayerA++;
+            }
+
+            else
+            {
+                _gameScorePlayerB++;
+            }  
+        }
+    }
+
+
+
+}
+
---
+++ TennisUnitTestProject/TennisUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using Tennis;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class TennisUnitTest
+    {
+        [TestCase(0, 0, "Love - All")]
+        [TestCase(1, 1, "Fifteen - All")]
+        [TestCase(2, 2, "Thirty - All")]
+        [TestCase(2, 3, "Thirty - Forty")]
+        [TestCase(3, 2, "Forty - Thirty")]
+        [TestCase(3, 3, "Forty - All")]
+        [TestCase(3, 4, "Advantage PlayerB")]
+        [TestCase(4, 3, "Advantage PlayerA")]
+        [TestCase(4, 5, "Advantage PlayerB")]
+        [TestCase(5, 4, "Advantage PlayerA")]
+        [TestCase(15, 14, "Advantage PlayerA")]
+        [TestCase(6, 4, "Win PlayerA")]
+        [TestCase(4, 6, "Win PlayerB")]
+        public void CheckSore(int scoreA, int scoreB , string result )
+        {
+            TennisGame myTennisGame = new TennisGame();
+            this.IncrementPlayer(Player.PlayerA, myTennisGame, scoreA);
+            this.IncrementPlayer(Player.PlayerB, myTennisGame, scoreB);
+            Assert.AreEqual(result, myTennisGame.GameScore.ToString());
+        }
+
+        public void IncrementPlayer(Player p , TennisGame tg, int score)
+        {
+            for (int i = 0; i < score; i++)
+                tg.AchievesScore(p);
+        }
+    }
+}
